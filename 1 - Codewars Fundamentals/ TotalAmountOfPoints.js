4e8e47f1252138257bb5c58eb92d7b7cfa32d5a0
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-03-23 08:37:12 -0500

    TotalAmountOfPoints

---
+++ 1 - Codewars Fundamentals/ TotalAmountOfPoints.js
@@ -0,0 +1,55 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Our football team has finished the championship.
+
+Our team's match results are recorded in a collection of strings. Each match is represented by a string in the format "x:y", where x is our team's score and y is our opponents score.
+
+For example: ["3:1", "2:2", "0:1", ...]
+
+Points are awarded for each match as follows:
+
+if x > y: 3 points (win)
+if x < y: 0 points (loss)
+if x = y: 1 point (tie)
+We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+
+Notes:
+
+our team always plays 10 matches in the championship
+0 <= x <= 4
+0 <= y <= 4
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function points(games) {
+    let totalPoints = 0;
+  
+    for (let i = 0; i < games.length; i++) {
+      const [x, y] = games[i].split(':').map(Number); 
+      if (x > y) {
+        totalPoints += 3; 
+      } else if (x === y) {
+        totalPoints += 1; 
+      }
+    }
+    return totalPoints; 
+  
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+const points=games=>games.reduce((output,current)=>{
+    return output += current[0]>current[2] ? 3 : current[0]===current[2] ? 1 : 0;
+  },0)
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I learned how to loop through game results, split strings to get the scores, and use if statements to calculate points based on wins, draws, or losses.
+
+
+*/

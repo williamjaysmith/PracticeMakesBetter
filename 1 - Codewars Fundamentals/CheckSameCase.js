82f40ade6d9fe803a904d330355c3983cdaad3ab
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-03-24 08:01:27 -0500

    CheckSameCase

---
+++ 1 - Codewars Fundamentals/CheckSameCase.js
@@ -0,0 +1,66 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Write a function that will check if two given characters are the same case.
+
+If either of the characters is not a letter, return -1
+If both characters are the same case, return 1
+If both characters are letters, but not the same case, return 0
+Examples
+
+'a' and 'g' returns 1
+
+'A' and 'C' returns 1
+
+'b' and 'G' returns 0
+
+'B' and 'g' returns 0
+
+'0' and '?' returns -1
+
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function sameCase(a, b) {
+    if (
+        a.toUpperCase() === a.toLowerCase() ||
+        b.toLowerCase() === b.toUpperCase()
+    ) {
+        return -1;
+    } else if (
+        (a === a.toLowerCase() && b === b.toLowerCase()) ||
+        (a === a.toUpperCase() && b === b.toUpperCase())
+    ) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function sameCase(a, b){
+ if(a.toUpperCase() === a.toLowerCase() || b.toLowerCase() === b.toUpperCase()){
+    return -1
+  }else if(a === a.toLowerCase() && b === b.toLowerCase() || a === a.toUpperCase() && b === b.toUpperCase()){
+      return 1
+  }else{
+    return 0
+  }
+}
+
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I learned how to check if two characters are both letters and whether they're the same case. If one or both aren’t letters, I return -1. If they’re both the same case, I return 1, and if they’re letters but different cases, I return 0. This helped me practice handling different character comparisons.
+
+
+
+
+
+
+*/

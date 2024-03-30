ebc1f4f57be06ac40b787ea9457ca692b92d542c
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-03-30 13:49:39 -0500

    RemoveDuplicates

---
+++ 1 - Codewars Fundamentals/RemoveDuplicates.js
@@ -0,0 +1,36 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+The order of the sequence has to stay the same.
+
+Examples:
+
+Input -> Output
+[1, 1, 2] -> [1, 2]
+[1, 2, 1, 1, 3, 2] -> [1, 2, 3]
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function distinct(a) {
+    return [...new Set(a)];
+  } 
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function distinct(a) {
+  return [...new Set(a)];
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I used a Set to automatically filter out duplicates and then turned it back into an array using the spread operator.
+
+
+*/

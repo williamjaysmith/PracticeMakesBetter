140c483ff73f03cfa44965022bd43e9037008760
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-03-31 11:56:07 -0500

    ValidateCode

---
+++ 1 - Codewars Fundamentals/ValidateCode.js
@@ -0,0 +1,34 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Basic regex tasks. Write a function that takes in a numeric code of any length. The function should check if the code begins with 1, 2, or 3 and return true if so. Return false otherwise.
+
+You can assume the input will always be a number.
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function validateCode(code) {
+    var codeSplit = code.toString().split("");
+
+    if (codeSplit[0] == "1" || codeSplit[0] == "2" || codeSplit[0] == "3") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function validateCode (code) {return /^[123]/.test(code)}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Learned that I didnt know what regex means ... and solved it a different way. 
+
+
+*/

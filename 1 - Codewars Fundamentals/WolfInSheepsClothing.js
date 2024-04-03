e6d80b0a4217d08f73d6b3d96555fd03f9937ba9
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-04-03 07:22:00 -0500

    WolfInSheepsClothing

---
+++ 1 - Codewars Fundamentals/WolfInSheepsClothing.js
@@ -0,0 +1,55 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Description:
+Wolves have been reintroduced to Great Britain. You are a sheep farmer, and are now plagued by wolves which pretend to be sheep. Fortunately, you are good at spotting them.
+
+Warn the sheep in front of the wolf that it is about to be eaten. Remember that you are standing at the front of the queue which is at the end of the array:
+
+[sheep, sheep, sheep, sheep, sheep, wolf, sheep, sheep]      (YOU ARE HERE AT THE FRONT OF THE QUEUE)
+   7      6      5      4      3            2      1
+If the wolf is the closest animal to you, return "Pls go away and stop eating my sheep". Otherwise, return "Oi! Sheep number N! You are about to be eaten by a wolf!" where N is the sheep's position in the queue.
+
+Note: there will always be exactly one wolf in the array.
+
+Examples
+Input: ["sheep", "sheep", "sheep", "wolf", "sheep"]
+Output: "Oi! Sheep number 1! You are about to be eaten by a wolf!"
+
+Input: ["sheep", "sheep", "wolf"]
+Output: "Pls go away and stop eating my sheep"
+
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function warnTheSheep(queue) {
+    const wolfIndex = queue.indexOf('wolf');
+  
+  if (wolfIndex === queue.length - 1) {
+    return "Pls go away and stop eating my sheep";
+  } else {
+    const sheepNumber = queue.length - 1 - wolfIndex;
+    return `Oi! Sheep number ${sheepNumber}! You are about to be eaten by a wolf!`;
+  }
+}
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function warnTheSheep(queue) {
+  const position = queue.reverse().indexOf('wolf');
+  return position === 0 ? 'Pls go away and stop eating my sheep' : `Oi! Sheep number ${ position }! You are about to be eaten by a wolf!`;
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I learned how to find the position of the wolf in an array and use that to decide which message to return. If the wolf is at the end, I warn it directly. If it's not, I figure out how many sheep are between me and the wolf to warn the right sheep.
+
+
+
+*/

da9f62c182b6073ca32f30b85c6be7d6815759c8
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-04-04 07:42:29 -0500

    ReversingWords

---
+++ 1 - Codewars Fundamentals/ReversingWords.js
@@ -0,0 +1,36 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+You need to write a function that reverses the words in a given string. Words are always separated by a single space.
+
+As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
+
+Example (Input --> Output)
+
+"Hello World" --> "World Hello"
+"Hi There." --> "There. Hi"
+Happy coding!
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function reverse(string) {
+    return string.trim().split(" ").reverse().join(" ");
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function reverse(string){
+  return string.split(' ').reverse().join(' ');
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I learned how to reverse the order of words in a string by using JavaScript functions like split(), reverse(), and join(). I also learned how to use trim() to remove extra spaces from the input.
+
+
+*/

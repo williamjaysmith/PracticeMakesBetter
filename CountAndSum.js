4a02de2b97bfc78a35b0fce0ec7b82a5796d7da7
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-04-06 10:58:41 -0500

    CountAndSum

---
+++ CountAndSum.js
@@ -0,0 +1,63 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function countPositivesSumNegatives(input) {
+    if (!input || input.length === 0) {
+        return [];
+    }
+
+    let countPositives = 0;
+    let sumNegatives = 0;
+
+    input.forEach((num) => {
+        if (num > 0) {
+            countPositives++;
+        } else if (num < 0) {
+            sumNegatives += num;
+        }
+    });
+
+    return [countPositives, sumNegatives];
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function countPositivesSumNegatives(input) {
+    if (input == null || input.length == 0)
+      return [];
+    
+    var positive = 0;
+    var negative = 0;
+    
+    for (var i=0, l=input.length; i<l; ++i)
+    {
+      if (input[i] > 0)
+        ++ positive;
+      else
+        negative += input[i];
+    }
+    
+    return [positive, negative];
+}
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+ I learned how to handle arrays and check for conditions like positive or negative numbers. I used a loop to count the positives and sum up the negatives. I also practiced handling edge cases, like when the input is empty or null, by returning an empty array.
+
+
+*/

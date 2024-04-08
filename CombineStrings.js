bbd21a2e28b9d27a3054fb0521d998ee854af509
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-04-08 06:45:26 -0500

    CombineStrings

---
+++ CombineStrings.js
@@ -0,0 +1,31 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Combine strings function
+Create a function named combineNames/combine_names/CombineNames that accepts two parameters (first and last name). The function should return the full name.
+
+Example:
+
+With "James" as the first name and "Stevens" as the last name should return "James Stevens"
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function combineNames(firstName, lastName) {
+    return firstName + " " + lastName;
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+const combineNames = (...names) => names.join(' ');
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+This taught me how to handle string concatenation
+
+
+*/

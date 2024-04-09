c3b15f636bdf11a77e80875c554e8dda20ab4fdd
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-04-09 07:30:54 -0500

    IsHeGunnaSurvive

---
+++ IsHeGunnaSurvive.js
@@ -0,0 +1,30 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+Return true if yes, false otherwise :)
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function hero(bullets, dragons) {
+    return bullets >= dragons * 2;
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function hero(bullets, dragons){
+  return bullets >= dragons * 2
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I learned to compare values by checking if the hero has at least twice as many bullets as there are dragons
+
+
+*/

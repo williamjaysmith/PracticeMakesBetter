e8f42c4b6c0c7578a2ef49df5a619f756995fe0d
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-04-10 07:11:17 -0500

    SayHello

---
+++ sayHello.js
@@ -0,0 +1,31 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Debugging sayHello function
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Example output:
+
+Hello, Mr. Spock
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function sayHello(name) {
+    return "Hello, " + name;
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+const sayHello = name => `Hello, ${name}`;
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I learned how to fix a function so it can greet someone by name. I made sure the name gets added to the greeting and saw how to use a function's input to create a message. 
+
+
+*/

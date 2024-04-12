7093173164c92d1d49514eec88eb215dc96ae68b
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-04-12 08:39:39 -0500

    alternate case

---
+++ 1 - Codewars Fundamentals/array-plus-array.js
@@ -0,0 +1,33 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
+
+P.S. Each array includes only integer numbers. Output is a number too.
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function arrayPlusArray(arr1, arr2) {
+    return (
+        arr1.reduce((acc, curr) => acc + curr, 0) +
+        arr2.reduce((acc, curr) => acc + curr, 0)
+    );
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function arrayPlusArray(arr1, arr2) {
+  return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+i learned how to use the reduce function to get the sum of an arrays elements
+
+
+*/

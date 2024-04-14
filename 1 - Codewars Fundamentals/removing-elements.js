f131acb7618d50f2888332332387e1be27d91cfc
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-04-14 14:22:22 -0500

    removing elements

---
+++ 1 - Codewars Fundamentals/removing-elements.js
@@ -0,0 +1,37 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+None of the arrays will be empty, so you don't have to worry about that!
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function removeEveryOther(arr) {
+    return arr.filter((number, index) => {
+        return index % 2 == 0;
+    });
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function removeEveryOther(arr){
+  return arr.filter(function(elem, index) {
+    return index % 2 === 0;
+  });
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I learned that in an arrow function, if I use curly braces, I need to include a return statement, but if I don't use curly braces, the return is automatic.
+
+
+*/

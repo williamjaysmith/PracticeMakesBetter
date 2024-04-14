bf0d74beace2d16da1f8756c8600da2e172f6ea0
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-04-14 14:21:58 -0500

    count sum

---
+++ 1 - Codewars Fundamentals/count-sum.js
@@ -0,0 +1,63 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function countPositivesSumNegatives(input) {
+    if (!input || input.length === 0) return [];
+    let positives = [];
+    let negatives = [];
+    input.forEach((element) => {
+        if (element > 0) {
+            positives.push(element);
+        } else if (element < 0) {
+            negatives.push(element);
+        }
+    });
+    let count = positives.length;
+    let sum = negatives.reduce((a, b) => {
+        return a + b;
+    }, 0);
+    return [count, sum];
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function countPositivesSumNegatives(input) {
+    if (input == null || input.length == 0)
+      return [];
+    
+    var positive = 0;
+    var negative = 0;
+    
+    for (var i=0, l=input.length; i<l; ++i)
+    {
+      if (input[i] > 0)
+        ++ positive;
+      else
+        negative += input[i];
+    }
+    
+    return [positive, negative];
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I learned how to count positive numbers and sum negative numbers in an array, and handle cases where the input is empty or null.
+Also to slow down and read the directions
+
+*/

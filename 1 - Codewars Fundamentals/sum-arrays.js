f0677a0a6ebf86b5ed8f9f5719ea5c8efefb9f03
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-04-17 18:43:25 -0500

    sum arrays

---
+++ 1 - Codewars Fundamentals/sum-arrays.js
@@ -0,0 +1,32 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
+
+P.S. Each array includes only integer numbers. Output is a number too.
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function arrayPlusArray(arr1, arr2) {
+    let arr1Sum = arr1.reduce((a, b) => a + b, 0);
+    let arr2Sum = arr2.reduce((a, b) => a + b, 0);
+    return arr1Sum + arr2Sum;
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function arrayPlusArray(arr1, arr2) {
+  return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I ultimately learned that i could have concatonated the two array first, before reducing.
+
+
+*/

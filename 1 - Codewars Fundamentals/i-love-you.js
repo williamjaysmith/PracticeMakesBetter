4dbe6d744015ab1d3baa4d438aa7652e8a39adcb
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-04-19 07:41:53 -0500

    i love you

---
+++ 1 - Codewars Fundamentals/i-love-you.js
@@ -0,0 +1,55 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
+
+"I love you"
+"a little"
+"a lot"
+"passionately"
+"madly"
+"not at all"
+If there are more than 6 petals, you start over with "I love you" for 7 petals, "a little" for 8 petals and so on.
+
+When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+
+Your goal in this kata is to determine which phrase the girls would say at the last petal for a flower of a given number of petals. The number of petals is always greater than 0.
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function howMuchILoveYou(nbPetals) {
+    let responses=["I love you",
+                   "a little",
+                   "a lot",
+                   "passionately",
+                   "madly",
+                   "not at all"];
+  return responses[(nbPetals-1)%responses.length]}
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+const phrases = [
+    'I love you',
+    'a little',
+    'a lot',
+    'passionately',
+    'madly',
+    'not at all',
+]
+
+function howMuchILoveYou(n) {
+     return phrases[(n - 1) % phrases.length] 
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I learned that arrays start at index 0, so I used nbPetals - 1 to adjust for that. The modulus operator (%) helped me cycle through the array when the number of petals is larger than the number of responses.
+
+
+*/

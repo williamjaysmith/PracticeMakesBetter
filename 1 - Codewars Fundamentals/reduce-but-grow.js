a51afd27694fcaac9c6a17bfe9669a23bdad8825
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-04-22 07:58:21 -0500

    reduce but grow

---
+++ 1 - Codewars Fundamentals/reduce-but-grow.js
@@ -0,0 +1,32 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function grow(x) {
+    let count = 1;
+    x.forEach((el) => {
+        count *= el;
+    });
+    return count;
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+const grow=x=> x.reduce((a,b) => a*b);
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+learned there was an easier way to *= using reduce
+
+
+*/

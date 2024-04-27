8f9a86de91f25f01a4c07b7515345bb35f05b9c7
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-04-27 12:34:26 -0500

    well of ideas

---
+++ 1 - Codewars Fundamentals/well-of-ideas.js
@@ -0,0 +1,40 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+For every good kata idea there seem to be quite a few bad ones!
+
+In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function well(x) {
+    let good = x.filter((el) => el == "good");
+
+    if (good.length > 0 && good.length <= 2) {
+        return "Publish!";
+    } else if (good.length > 2) {
+        return "I smell a series!";
+    } else {
+        return "Fail!";
+    }
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+const well = x => {
+  const good_count = x.filter(x => x == 'good').length;
+  return good_count < 1 ? 'Fail!' : 
+         good_count < 3 ? 'Publish!' : 'I smell a series!';
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I learned how to use the filter() method to count specific elements in an array and return different results based on that count.
+
+
+*/

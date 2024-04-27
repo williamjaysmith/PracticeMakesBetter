c01e87373cedab1e5d900c3fb99ae471559a8c93
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-04-27 16:43:45 -0500

    count the monkey

---
+++ 1 - Codewars Fundamentals/ count-the-monkeys.js
@@ -0,0 +1,43 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+You take your son to the forest to see the monkeys. You know that there are a certain number there (n), but your son is too young to just appreciate the full number, he has to start counting them from 1.
+
+As a good parent, you will sit and count with him. Given the number (n), populate an array with all numbers up to and including that number, but excluding zero.
+
+For example(Input --> Output):
+
+10 --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ 1 --> [1]
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function monkeyCount(n) {
+    let count = [];
+    for (let i = 1; i <= n; i++) {
+        count.push(i);
+    }
+    return count;
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function monkeyCount(n) {
+ var monkeys = [];
+ for(var i=1; i<n+1; i++){
+   monkeys.push(i);
+ }
+ return monkeys;
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Learned to remember to check that for loop uses ; and not , 
+
+
+*/

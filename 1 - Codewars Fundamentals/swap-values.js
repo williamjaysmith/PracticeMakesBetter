a0a8c51a2a81ccd4f87bb5c1f9e50ad6ee66be7d
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-05-04 17:28:47 -0500

    swap values

---
+++ 1 - Codewars Fundamentals/swap-values.js
@@ -0,0 +1,30 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I would like to be able to pass an array with two elements to my swapValues function to swap the values. However it appears that the values aren't changing.
+
+Can you figure out what's wrong here?
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function swapValues(arg) {
+    [arg[1], arg[0]] = [arg[0], arg[1]];
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function swapValues(arr) {
+    return arr.reverse();
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+learned there was a .reverse method to reverse the elements of an array
+
+
+*/

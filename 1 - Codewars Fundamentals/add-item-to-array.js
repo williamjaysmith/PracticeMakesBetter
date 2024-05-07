b12229c1da99aa0278cbb18d1a96e991231fa001
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-05-07 07:27:15 -0500

    add item to array

---
+++ 1 - Codewars Fundamentals/add-item-to-array.js
@@ -0,0 +1,29 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Add the value "codewars" to the websites array.
+After your code executes the websites array should == ["codewars"]
+
+The websites array has already been defined for you using the following code:
+
+var websites = [];
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+websites.push("codewars")
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+websites.push("codewars")
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+cannot believe how much trouble this gave me because i misunderstood the request. Learned to slow down and try to understand whats needed
+
+
+*/

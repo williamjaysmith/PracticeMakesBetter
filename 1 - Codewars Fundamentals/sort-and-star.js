25bfad5fef66f8660f817f66987cb30efe53289d
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-05-12 17:20:03 -0500

    sort and star

---
+++ 1 - Codewars Fundamentals/sort-and-star.js
@@ -0,0 +1,32 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+The returned value must be a string, and have "***" between each of its letters.
+
+You should not remove or add elements from/to the array.
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function twoSort(s) {
+    return s.sort()[0].split("").join("***");
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function twoSort(s) {
+   return s.sort()[0].split('').join('***')
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+ i learned to find and pull an element from its array based on its index within the alphabetical order
+
+
+*/

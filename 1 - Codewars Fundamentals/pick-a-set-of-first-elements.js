bcbd304dff5ec001b0aa30fdbbf0c8a424c37cc6
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-05-13 20:10:50 -0500

    pick a set of first elements

---
+++ 1 - Codewars Fundamentals/pick-a-set-of-first-elements.js
@@ -0,0 +1,38 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Write a function to get the first element(s) of a sequence. Passing a parameter n (default=1) will return the first n element(s) of the sequence.
+
+If n == 0 return an empty sequence []
+
+Examples
+var arr = ['a', 'b', 'c', 'd', 'e'];
+first(arr) //=> ['a'];
+first(arr, 2) //=> ['a', 'b']
+first(arr, 3) //=> ['a', 'b', 'c'];
+first(arr, 0) //=> [];
+
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function first(arr, n = 1) {
+    return n === 0 ? [] : arr.slice(0, n);
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function first(arr, n=1) {
+   return arr.slice(0,n);
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+i learned to read the directions more carefully and ended up realizing i needed to default n to 1, so if nothing is passed we'll still get 'a'
+
+
+*/

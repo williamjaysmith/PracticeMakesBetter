fc9ebe95e2b1b7a95e5b80637a6e28f1e2d4c257
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-05-18 15:56:21 -0500

    take an arrow to the knee

---
+++ 1 - Codewars Fundamentals/take-an-arrow-to-the-knee.js
@@ -0,0 +1,32 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+You will be given an array of numbers which can be used using the String.fromCharCode() (JS), Tools.FromCharCode() (C#) method to convert the number to a character. It is recommended to map over the array of numbers and convert each number to the corresponding ascii character.
+
+Examples
+These are example of how to convert a number to an ascii Character:
+Javascript => String.fromCharCode(97) // a
+C# => Tools.FromCharCode(97) // a
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+var ArrowFunc = function (arr) {
+    return arr.map((el) => String.fromCharCode(el)).join(""); //Complete this function
+};
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+var ArrowFunc = function(arr) {
+  return arr.map( x => String.fromCharCode(x) ).join(''); 
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+this was a strange one because they gave us the answer right in the description.. learned what fromCharCode is
+
+*/

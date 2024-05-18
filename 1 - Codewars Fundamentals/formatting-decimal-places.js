bf4e1cbac19efeb59ad51333453402e7e7464688
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-05-17 20:16:59 -0500

    formatting decimal places

---
+++ 1 - Codewars Fundamentals/formatting-decimal-places.js
@@ -0,0 +1,31 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Each number should be formatted that it is rounded to two decimal places. You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+
+Example:    
+5.5589 is rounded 5.56   
+-3.3424 is rounded -3.34
+
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function twoDecimalPlaces(n) {
+    return Number(n.toFixed(2));
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+const twoDecimalPlaces = n =>  Number(n.toFixed(2))
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+learned that toFixed returns a string and needs to be converted back to a number
+
+
+*/

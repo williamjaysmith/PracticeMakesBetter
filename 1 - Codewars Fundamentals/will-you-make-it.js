4879231374d18f08e03a95a647c7ef1903b7b8f2
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-05-19 19:47:59 -0500

    will you make it

---
+++ 1 - Codewars Fundamentals/will-you-make-it.js
@@ -0,0 +1,32 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+
+Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+
+Function should return true if it is possible and false if not.
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+    return fuelLeft * mpg >= distanceToPump;
+};
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  return distanceToPump/mpg <= fuelLeft
+};
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+For a second , i tried to divide fuelLeft by mpg, mixing that up in my head. Quick recovery though.
+
+
+*/

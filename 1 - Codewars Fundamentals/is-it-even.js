285d42e53bee38b1813d1f8c9ccad92229926f4f
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-05-25 17:35:17 -0500

    is it even

---
+++ 1 - Codewars Fundamentals/is-it-even.js
@@ -0,0 +1,36 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+In this Kata we are passing a number (n) into a function.
+
+Your code will determine if the number passed is even (or not).
+
+The function needs to return either a true or false.
+
+Numbers may be positive or negative, integers or floats.
+
+Floats with decimal part non equal to zero are considered UNeven for this kata.
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function testEven(n) {
+    return n % 2 == 0 ? true : false;
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function testEven(n) {
+    return n%2===0;
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+learning that the expression itself will resolve to true or false and doesnt need extra syntax
+
+
+*/

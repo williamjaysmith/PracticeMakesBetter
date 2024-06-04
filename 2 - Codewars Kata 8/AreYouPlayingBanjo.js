687a71d8bbc8b15909f94aa6b9fbd52f9d4fec49
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-06-04 07:24:33 -0500

    AreYouPlayingBanjo

---
+++ 2 - Codewars Kata 8/AreYouPlayingBanjo.js
@@ -0,0 +1,39 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Create a function which answers the question "Are you playing banjo?".
+If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+The function takes a name as its only argument, and returns one of the following strings:
+
+name + " plays banjo" 
+name + " does not play banjo"
+Names given are always valid strings.
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function areYouPlayingBanjo(name) {
+    if (name[0] == "r" || name[0] == "R") {
+        return name + " plays banjo";
+    } else {
+        return name + " does not play banjo";
+    }
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function areYouPlayingBanjo(name) {
+  return name + (name[0].toLowerCase() == 'r' ? ' plays' : ' does not play') + " banjo";
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I learned how to check if a name starts with "R" (or "r") using a conditional (ternary) operator to return different messages based on the first letter of the name.
+
+
+*/

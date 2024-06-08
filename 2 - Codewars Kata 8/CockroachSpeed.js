a27f155b49932ae5152917776e84fac60d26acbd
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-06-08 12:14:33 -0500

    CockroachSpeed

---
+++ 2 - Codewars Kata 8/CockroachSpeed.js
@@ -0,0 +1,31 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+
+For example:
+
+1.08 --> 30
+Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer.
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function cockroachSpeed(s) {
+    return Math.floor(s * 27.777778);
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+const cockroachSpeed = s => Math.floor(s / 0.036);
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I learned how to convert speed from meters per second to centimeters per second by multiplying by a number and then rounding down to the nearest whole number using Math.floor().
+
+
+*/

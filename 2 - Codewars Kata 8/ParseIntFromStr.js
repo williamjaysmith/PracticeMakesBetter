066f4c127f5ab0a0bcdb93fb27254be8826b663e
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-06-11 07:35:14 -0500

    ParseIntFromStr

---
+++ 2 - Codewars Kata 8/ParseIntFromStr.js
@@ -0,0 +1,33 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+
+Write a program that returns the girl's age (0-9) as an integer.
+
+Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
+
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function getAge(inputString) {
+    return parseInt(inputString[0]);
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function getAge(inputString){
+  return parseInt(inputString);
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I learned how to extract and convert the first character of a string to an integer using parseInt().
+
+
+*/

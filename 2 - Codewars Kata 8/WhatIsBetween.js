e6b6f0e734dd800106d832827f2a4b3389df21d6
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-06-12 20:45:40 -0500

    WhatIsBetween

---
+++ 2 - Codewars Kata 8/WhatIsBetween.js
@@ -0,0 +1,42 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+For example:
+
+a = 1
+b = 4
+--> [1, 2, 3, 4]
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function between(a, b) {
+    let output = [];
+    for (let i = a; i <= b; i++) {
+        output.push(i);
+    }
+    return output;
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function between(a, b) {
+    let output = [];
+    for (let i = a; i <= b; i++) {
+        output.push(i);
+    }
+    return output;
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I learned how to generate an array of consecutive integers between two given values by using a loop to add each integer to an array.
+
+
+*/

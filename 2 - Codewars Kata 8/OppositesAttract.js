ccdf6a5d4683241e727ab6048a4f2b1581936cba
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-06-13 19:26:13 -0500

    OppositesAttract

---
+++ 2 - Codewars Kata 8/OppositesAttract.js
@@ -0,0 +1,34 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function lovefunc(flower1, flower2) {
+    return (
+        (flower1 % 2 == 0 && flower2 % 2 == 1) ||
+        (flower1 % 2 == 1 && flower2 % 2 == 0)
+    );
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function lovefunc(flower1, flower2){
+  return flower1 % 2 !== flower2 % 2;
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I learned how to determine if two numbers are of different parity (one even and one odd) using the modulus operator and logical conditions.
+
+
+*/

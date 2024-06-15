a7928cd517f3a090c135758788618e07bcde49a2
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-06-15 15:49:25 -0500

    ConvertBoolean

---
+++ 2 - Codewars Kata 8/ConvertBoolean.js
@@ -0,0 +1,29 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Implement a function which convert the given boolean value into its string representation.
+Note: Only valid inputs will be given.
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function booleanToString(b) {
+    return b.toString();
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function booleanToString(b){
+    return b.toString();
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I learned how to convert a boolean value (true or false) to a string using the toString() method.
+
+
+*/

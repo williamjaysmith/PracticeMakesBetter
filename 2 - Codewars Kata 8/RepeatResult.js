1c3ea17da75d74d5cbe11196db4ea63ee3ad666b
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-06-16 10:34:21 -0500

    RepeatResult

---
+++ 2 - Codewars Kata 8/RepeatResult.js
@@ -0,0 +1,33 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function repeatStr(n, s) {
+    let answer = "";
+    for (let i = 0; i < n; i++) {
+        answer += s;
+    }
+    return answer;
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I learned how to repeat a string a specified number of times by concatenating the string in a loop and then returning the final repeated result.
+
+
+*/

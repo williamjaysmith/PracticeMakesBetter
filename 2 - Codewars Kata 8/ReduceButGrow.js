5d5bba175faeacca55a000062155a20f9e7326bd
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-06-17 07:41:05 -0500

    ReduceButGrow

---
+++ 2 - Codewars Kata 8/ReduceButGrow.js
@@ -0,0 +1,30 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Given a non-empty array of integers, return the result of multiplying the values together in order. 
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function grow(num) {
+    const total = num.reduce(
+        (arrayNet, arrayValue) => arrayNet * arrayValue,
+        1
+    );
+    return total;
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+const grow=x=> x.reduce((a,b) => a*b);
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I learned how to multiply all the elements in an array together using the reduce() method, starting with an initial value of 1.
+
+
+*/

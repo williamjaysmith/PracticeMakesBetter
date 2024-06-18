2fcf070528eea0ab8cbbadae16e29c07293da8ad
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-06-18 07:27:36 -0500

    FindSmallerInteger

---
+++ 2 - Codewars Kata 8/FindSmallestInteger.js
@@ -0,0 +1,36 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Given an array of integers your solution should find the smallest integer.
+For example:
+Given [34, 15, 88, 2] your solution will return 2
+Given [34, -345, -1, 100] your solution will return -345
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+        return Math.min.apply(Math, args);
+    }
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args)
+  }
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I learned how to find the smallest integer in an array by using the Math.min() function along with apply() to pass the array elements as individual arguments.
+
+
+*/

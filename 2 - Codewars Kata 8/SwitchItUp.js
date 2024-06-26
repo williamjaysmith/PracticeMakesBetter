2e7635d94c1a0341e6ec48abca1914f288dc28d6
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-06-25 20:42:35 -0500

    SwitchedUp

---
+++ 2 - Codewars Kata 8/SwitchItUp.js
@@ -0,0 +1,67 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+When provided with a number between 0-9, return it in words.
+
+Input :: 1
+
+Output :: "One".
+
+If your language supports it, try using a switch statement.
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function switchItUp(number) {
+    switch (number) {
+        case 0:
+            return "Zero";
+            break;
+        case 1:
+            return "One";
+            break;
+        case 2:
+            return "Two";
+            break;
+        case 3:
+            return "Three";
+            break;
+        case 4:
+            return "Four";
+            break;
+        case 5:
+            return "Five";
+            break;
+        case 6:
+            return "Six";
+            break;
+        case 7:
+            return "Seven";
+            break;
+        case 8:
+            return "Eight";
+            break;
+        case 9:
+            return "Nine";
+            break;
+        default:
+            return "Invalid input";
+            break;
+    }
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+switchItUp=n=>["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"][n]
+
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I learned how to use a switch statement to return a string representation of a number from 0 to 9, and handle any invalid input by returning "Invalid input".
+
+
+*/

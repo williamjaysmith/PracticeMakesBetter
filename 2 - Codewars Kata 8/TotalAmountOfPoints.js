79c6854f45fd913ef90a0cf7be4ee791cecffca1
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-06-29 17:46:59 -0500

    TotalAmountOfPoints

---
+++ 2 - Codewars Kata 8/TotalAmountOfPoints.js
@@ -0,0 +1,55 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Our football team has finished the championship.
+
+Our team's match results are recorded in a collection of strings. Each match is represented by a string in the format "x:y", where x is our team's score and y is our opponents score.
+
+For example: ["3:1", "2:2", "0:1", ...]
+
+Points are awarded for each match as follows:
+
+if x > y: 3 points (win)
+if x < y: 0 points (loss)
+if x = y: 1 point (tie)
+We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+
+Notes:
+
+our team always plays 10 matches in the championship
+0 <= x <= 4
+0 <= y <= 4
+
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function points(games) {
+    let seasonTotal = 0;
+
+    for (let i = 0; i < games.length; i++) {
+        games[i][0] == games[i][2]
+            ? (seasonTotal += 1)
+            : games[i][0] > games[i][2]
+            ? (seasonTotal += 3)
+            : 0;
+    }
+    return seasonTotal;
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+const points=games=>games.reduce((output,current)=>{
+    return output += current[0]>current[2] ? 3 : current[0]===current[2] ? 1 : 0;
+  },0)
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I learned how to calculate the total points earned in a series of games based on the results. If the game is a draw, 1 point is added; if the game is won, 3 points are added; and if it's lost, no points are added.
+
+
+*/

8a782e236d4b389430bf6acdee8aabadaa277c87
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-06-30 11:36:18 -0500

    ReturningString

---
+++ 2 - Codewars Kata 8/ReturningString.js
@@ -0,0 +1,30 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function greet(name) {
+    return `Hello, ${name} how are you doing today?`;
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function greet(name){
+    return `Hello, ${name} how are you doing today?`;
+  }
+  
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+
+I learned how to use template literals to create a personalized greeting by embedding a variable within a string.
+
+
+*/

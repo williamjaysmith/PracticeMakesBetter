ad925460e3b634ee84c02968683a9815115bdf87
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-07-01 08:44:45 -0500

    CountingSheep

---
+++ 2 - Codewars Kata 8/CountingSheep.js
@@ -0,0 +1,38 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+For example,
+
+[true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true]
+The correct answer would be 17.
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function countSheeps(sheep) {
+    return sheep.filter(Boolean).length;
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function countSheeps(sheep) {
+    return sheep.filter(Boolean).length;
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I learned how to count the number of true values in an array by using the filter() method to keep only the true values and then getting the length of the resulting array.
+
+
+*/

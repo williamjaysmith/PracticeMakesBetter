2e3706c659f76dd7357610c36eb8a7c424c6f1ed
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-07-03 08:25:51 -0500

    Summation

---
+++ 2 - Codewars Kata 8/Summation.js
@@ -0,0 +1,37 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0. Your function only needs to return the result, what is shown between parentheses in the example below is how you reach that result and it's not part of it, see the sample tests.
+
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+var summation = function (num) {
+    let sum = 0;
+    for (let i = 0; i <= num; i++) {
+        sum += i;
+    }
+    return sum;
+};
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+var summation = function (num) {
+    let sum = 0
+    for (let i = 0; i <= num; i++){
+      sum += i;
+    }
+    return sum;
+  }
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I learned how to calculate the sum of all integers from 0 up to a given number using a loop to accumulate the total.
+
+
+*/

034b1f5bdfad1a060d5473ca9824cebc2d1b0e32
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-07-04 08:38:20 -0500

    TransportationOnVacation

---
+++ 2 - Codewars Kata 8/TransportationOnVacation.js
@@ -0,0 +1,35 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+After a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
+
+You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
+
+Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+
+Write a code that gives out the total amount for different days(d).
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function rentalCarCost(d) {
+    let totalCost = d * 40;
+    return d >= 7 ? (totalCost -= 50) : d >= 3 ? (totalCost -= 20) : totalCost;
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+unction rentalCarCost(d) {
+  return d * 40 - (d >= 7 ? 50 : (d >= 3 ? 20 : 0));
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I learned how to calculate the rental car cost with discounts based on the number of days rented. If rented for 7 days or more, a $50 discount is applied; if rented for 3 to 6 days, a $20 discount is applied; otherwise, no discount is given.
+
+
+*/

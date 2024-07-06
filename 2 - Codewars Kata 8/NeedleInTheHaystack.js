d9271adc965c8313ecc91d8012616661c7b2ad9d
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-07-06 08:38:55 -0500

    NeedleInTheHaystack

---
+++ 2 - Codewars Kata 8/NeedleInTheHaystack.js
@@ -0,0 +1,36 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Can you find the needle in the haystack?
+
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+After your function finds the needle it should return a message (as a string) that says:
+
+"found the needle at position " plus the index it found the needle
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function findNeedle(haystack) {
+    let position = haystack.findIndex((element) => element == "needle");
+
+    return `found the needle at position ${position}`;
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function findNeedle(haystack) {
+  return "found the needle at position " + haystack.indexOf("needle");
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I learned how to find the position of a specific item in an array by using the findIndex() method and then return a formatted string indicating the position of that item.
+
+
+*/

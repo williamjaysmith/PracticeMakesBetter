d3294091877c168169b1e0621b6777cba5dd5b5c
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-07-09 08:45:58 -0500

    AllStarChallenge

---
+++ 2 - Codewars Kata 8/AllStarChallenge.js
@@ -0,0 +1,40 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Create a function that accepts a string and a single character, and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+
+If no occurrences can be found, a count of 0 should be returned.
+
+("Hello", "o")  ==>  1
+("Hello", "l")  ==>  2
+("", "z")       ==>  0
+str_count("Hello", 'o'); // returns 1
+str_count("Hello", 'l'); // returns 2
+str_count("", 'z'); // returns 0
+Notes
+The first argument can be an empty string
+In languages with no distinct character data type, the second argument will be a string of length 1
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function strCount(str, letter) {
+    return str.split(letter).length - 1;
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function strCount(str, letter){  
+    return str.split(letter).length-1;
+  }
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I learned how to count occurrences of a letter in a string using split(), which breaks the string at each instance of the letter, and subtracting 1 from the resulting array length gives the count.
+
+
+*/

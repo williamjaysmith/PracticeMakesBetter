17f0ed71eed212036e8c303ad0c2c70ec2b16f84
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-07-13 12:52:31 -0500

    alternate capital

---
+++ codewars/Kata 6/alternate-capitalization.js
@@ -0,0 +1,45 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+
+For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+
+The input will be a lowercase string with no spaces.
+
+Good luck!
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function capitalize(s) {
+    let capEvens = s
+        .split("")
+        .map((char, i) => (i % 2 === 0 ? char.toUpperCase() : char))
+        .join("");
+    let capOdds = s
+        .split("")
+        .map((char, i) => (i % 2 !== 0 ? char.toUpperCase() : char))
+        .join("");
+
+    return [capEvens, capOdds];
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function capitalize(s){
+  const odd = s.split("").map((l, i) => i % 2 !== 0 ? l.toUpperCase() : l).join("");
+  const even = s.split("").map((l, i) => i % 2 === 0 ? l.toUpperCase() : l).join("");
+  return [even, odd];
+};
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+i learned how to manipulated characters of a string based on their index of odd or even. 
+
+
+*/

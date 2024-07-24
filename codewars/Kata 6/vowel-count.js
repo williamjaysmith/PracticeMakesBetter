bb65770b18967091c741c240b3428c1e12ed80ee
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-07-24 07:49:33 -0500

    vowel count

---
+++ codewars/Kata 6/vowel-count.js
@@ -0,0 +1,41 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function getCount(str) {
+    const vowels = "aeiou";
+    let vowelCount = 0;
+    str.split("").map((char) => {
+        if (vowels.includes(char)) {
+            vowelCount += 1;
+        }
+    });
+    return vowelCount;
+}
+console.log(getCount("yall must be forklift certified"));
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function getCount(str) {
+  return (str.match(/[aeiou]/ig)||[]).length;
+}
+
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+
+
+
+*/

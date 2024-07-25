ed0444c62a7ed5a6f0b4caeef93aa67c816299cc
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-07-25 07:53:12 -0500

    string ends with

---
+++ codewars/Kata 6/string-ends-with.js
@@ -0,0 +1,33 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function solution(str, ending) {
+    return str.endsWith(ending);
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function solution(str, ending){
+  return str.endsWith(ending);
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+learned about the .endsWith method
+
+
+*/

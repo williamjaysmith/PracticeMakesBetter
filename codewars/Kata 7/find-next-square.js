cb92c52efea2b915f05c9bb92de791b5ec6614f7
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-07-28 20:08:13 -0500

    find-next-one

---
+++ codewars/Kata 7/find-next-square.js
@@ -0,0 +1,49 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+You might know some pretty large perfect squares. But what about the NEXT one?
+
+Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+If the argument is itself not a perfect square then return either -1 or an empty value like None or null, depending on your language. You may assume the argument is non-negative.
+
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function findNextSquare(sq) {
+    // Return the next square if sq is a perfect square, -1 otherwise
+    let root = Math.sqrt(sq);
+    if (root != Math.floor(root)) {
+        return -1;
+    } else {
+        let nxtRoot = Math.ceil(root) + 1;
+        return nxtRoot * nxtRoot;
+    }
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function findNextSquare(sq) {
+  return Math.sqrt(sq)%1? -1 : Math.pow(Math.sqrt(sq)+1,2);
+}
+
+
+function findNextSquare(sq) {
+  var number = Math.sqrt(sq);
+  if(Math.round(number) === number) {
+    return Math.pow(++number, 2)
+  }
+  return -1;
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+
+
+
+*/

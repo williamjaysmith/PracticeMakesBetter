c1b9fb708a1a4c4b686e94fa42ea4bf3c76308f0
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-08-01 08:34:03 -0500

    ConvertBoolean

---
+++ 2 - Codewars Kata 8/ConvertBooleanToYes.js
@@ -0,0 +1,28 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function boolToWord(bool) {
+    return bool ? "Yes" : "No";
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function boolToWord( bool ){
+    return bool ? 'Yes':'No';
+  }
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I learned how to convert a boolean value to a string by using a conditional (ternary) operator to return "Yes" for true and "No" for false.
+
+
+*/

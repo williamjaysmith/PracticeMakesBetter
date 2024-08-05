c4c938fbe3898b6580bed9d5304b1fda7402f677
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-08-04 20:20:47 -0500

    no oddities here

---
+++ codewars/Kata 7/no-oddities-here.js
@@ -0,0 +1,27 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Write a small function that returns the values of an array that are not odd.
+
+All values in the array will be integers. Return the good values in the order they are given.
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function noOdds(values) {
+    return values.filter((a) => a % 2 === 0);
+}
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+var noOdds = values => values.filter(x => x % 2 === 0);
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+practiced using filter to find even numbers only
+
+
+*/

7fdf311842294eb635ec97e5e45e5b0dadaf3d4a
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-08-06 07:14:40 -0500

    array combinations

---
+++ codewars/Kata 7/array-combinations.js
@@ -0,0 +1,34 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+In this Kata, you will be given an array of arrays and your task will be to return the number of unique arrays that can be formed by picking exactly one element from each subarray.
+
+For example: solve([[1,2],[4],[5,6]]) = 4, because it results in only 4 possibilites. They are [1,4,5],[1,4,6],[2,4,5],[2,4,6].
+
+Make sure that you don't count duplicates; for example solve([[1,2],[4,4],[5,6,6]]) = 4, since the extra outcomes are just duplicates.
+
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function solve(arr) {
+    return arr
+        .map((el) => [...new Set(el)])
+        .reduce((acc, cur) => acc * cur.length, 1);
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+const solve = a => a.map( v => new Set(v).size ).reduce( (v,w) => v*w , 1 ) ;,
+
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+ I learned that if i leave new Set without converting back into an array , i access its length by saying .size. And i learned to find all combination of array by multiplying the sub arrays.
+
+
+*/

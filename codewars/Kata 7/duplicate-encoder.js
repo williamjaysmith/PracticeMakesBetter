59325dd6c15525798aa96239f318b44637cd8452
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-08-11 17:36:04 -0500

    duplicate encoder

---
+++ codewars/Kata 7/duplicate-encoder.js
@@ -0,0 +1,61 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function duplicateEncode(word) {
+    let lower = word.toLowerCase();
+
+    let letterCount = {};
+    for (let char of lower) {
+        letterCount[char] = (letterCount[char] || 0) + 1;
+    }
+
+    let newWord = "";
+    for (let char of lower) {
+        newWord += letterCount[char] > 1 ? ")" : "(";
+    }
+
+    return newWord;
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function duplicateEncode(word){
+  return word
+    .toLowerCase()
+    .split('')
+    .map( function (a, i, w) {
+      return w.indexOf(a) == w.lastIndexOf(a) ? '(' : ')'
+    })
+    .join('');
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+
+*/

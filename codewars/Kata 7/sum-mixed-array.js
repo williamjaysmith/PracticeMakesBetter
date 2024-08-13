b7a92a3032a08760ed45aa272dbbf2f5876ab295
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-08-13 08:46:46 -0500

    sum mixed array

---
+++ codewars/Kata 7/sum-mixed-array.js
@@ -0,0 +1,30 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function sumMix(x) {
+    return x.reduce((acc, num) => acc + Number(num), 0);
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function sumMix(x){
+  return x.map(a => +a).reduce((a, b) => a + b);
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+got some practice with the reduce function
+
+
+*/

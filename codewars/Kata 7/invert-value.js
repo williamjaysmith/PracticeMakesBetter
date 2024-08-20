a737abc73c3177827c227f6a3ad12891e83a6d87
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-08-20 08:20:51 -0500

    invert value

---
+++ codewars/Kata 7/invert-value.js
@@ -0,0 +1,31 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+invert([]) == []
+You can assume that all values are integers. Do not mutate the input array/list.
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function invert(array) {
+    return array.map((x) => x * -1);
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+const invert = array => array.map(num => -num);
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+
+
+
+*/

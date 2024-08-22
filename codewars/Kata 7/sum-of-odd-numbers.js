96f92e6fd595f345904f9c731318baf506eb351a
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-08-22 08:06:28 -0500

    sum of odd numbers

---
+++ codewars/Kata 7/sum-of-odd-numbers.js
@@ -0,0 +1,36 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Given the triangle of consecutive odd numbers:
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+1 -->  1
+2 --> 3 + 5 = 8
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function rowSumOddNumbers(n) {
+    return Math.pow(n, 3);
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function rowSumOddNumbers(n) {
+    return Math.pow(n, 3);
+  }
+
+
+
+
+*/

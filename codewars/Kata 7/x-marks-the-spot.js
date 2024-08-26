d9818250608b638373140da6e217b46162af9466
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-08-25 19:42:20 -0500

    x marks the spot

---
+++ codewars/Kata 7/x-marks-the-spot.js
@@ -0,0 +1,82 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Given a two dimensional array, return the co-ordinates of x.
+
+If x is not inside the array, or if x appears multiple times, return [].
+
+The co-ordinates should be zero indexed in row-major order.
+You should assume you will always get an array as input. The array will only contain 'x's and 'o's.
+
+Example
+
+Input: [
+  ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+  ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+  ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+  ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+  ['o', 'o', 'o', 'o', 'o', 'o', 'x', 'o'],
+  ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o']
+]
+Return [4,6] for the example above => [4, 6]
+
+
+
+
+
+
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+const xMarksTheSpot = (input) => {
+    let coordinates = [];
+    for (let i = 0; i < input.length; i++) {
+        for (let j = 0; j < input[i].length; j++) {
+            if (input[i][j] === "x") {
+                coordinates.push([i, j]);
+            }
+        }
+    }
+    if (coordinates.length === 1) {
+        return coordinates[0];
+    } else {
+        return [];
+    }
+};
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+const xMarksTheSpot = (input) => {
+  let coords = []
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i].includes("x")) {
+      coords.push([i, input[i].indexOf("x")])
+    }
+  }
+  if (coords.length === 1) {
+    return coords[0]
+  } else {
+    return []
+  }
+}
+
+
+
+
+
+
+
+
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+
+
+
+*/

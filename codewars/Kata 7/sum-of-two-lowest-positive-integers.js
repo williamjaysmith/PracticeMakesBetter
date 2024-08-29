1f8ddb4bd1dab0f7b7ced5df52546653a7221fd9
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-08-29 17:51:17 -0500

    sum of two lowest positive integers

---
+++ codewars/Kata 7/sum-of-two-lowest-positive-integers.js
@@ -0,0 +1,34 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+[10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function sumTwoSmallestNumbers(numbers) {
+    let sorted = numbers.sort((a, b) => a - b);
+    return sorted[0] + sorted[1];
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function sumTwoSmallestNumbers(numbers){  
+  numbers = numbers.sort(function(a, b){return a - b; });
+  return numbers[0] + numbers[1];
+};
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+got some good practice using the sort method to extract and sum the two lowest numbers
+
+
+*/

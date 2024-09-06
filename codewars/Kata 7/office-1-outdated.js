61ae6f69db3c892a12d137dce1f7036841b04515
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-09-06 08:22:08 -0500

    office 1

---
+++ codewars/Kata 7/office-1-outdated.js
@@ -0,0 +1,47 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Your colleagues have been looking over your shoulder. When you should have been doing your boring real job, you've been using the work computers to smash in endless hours of codewars.
+
+In a team meeting, a terrible, awful person declares to the group that you aren't working. You're in trouble. You quickly have to gauge the feeling in the room to decide whether or not you should gather your things and leave.
+
+Given an object ( meet ) containing team member names as keys, and their happiness rating out of 10 as the value, you need to assess the overall happiness rating of the group. If <= 5, return 'Get Out Now!'. Else return 'Nice Work Champ!'.
+
+Happiness rating will be total score / number of people in the room.
+
+Note that your boss is in the room ( boss ). Their score is worth double its face value (but they are still just one person!).
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function outed(meet, boss) {
+    let team = 0;
+
+    for (let person in meet) {
+        team += meet[person];
+    }
+
+    team += meet[boss];
+
+    let avgRating = team / Object.keys(meet).length;
+
+    return avgRating <= 5 ? "Get Out Now!" : "Nice Work Champ!";
+}
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function outed(meet, boss) {
+  let names = Object.keys(meet)
+  let score = names.reduce((s,v) => s + meet[v], 0) + meet[boss]
+  return score / names.length > 5 ? 'Nice Work Champ!' : 'Get Out Now!'
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+i learned the Object.keys returns an array of keys. So i could have use the reduce method on the keys array instead of a loop.
+
+
+*/

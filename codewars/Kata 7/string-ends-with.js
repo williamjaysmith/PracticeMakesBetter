3abfa0216aabda5e198f4adc85975d7a61bd7db6
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-09-07 16:22:36 -0500

    string ends with

---
+++ codewars/Kata 7/string-ends-with.js
@@ -0,0 +1,26 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function solution(str, ending) {
+    return str.includes(ending, str.length - ending.length);
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function solution(str, ending){
+  return str.endsWith(ending);
+}
+
+
+
+
+*/

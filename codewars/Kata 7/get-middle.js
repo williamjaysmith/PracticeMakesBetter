0d08abdcb4a8e71e40e95474a3154ff63e48fe3c
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-10-29 07:28:13 -0500

    get middle

---
+++ codewars/Kata 7/get-middle.js
@@ -0,0 +1,41 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+You are going to be given a non-empty string. Your job is to return the middle character(s) of the string.
+
+If the string's length is odd, return the middle character.
+If the string's length is even, return the middle 2 characters.
+Examples:
+"test" --> "es"
+"testing" --> "t"
+"middle" --> "dd"
+"A" --> "A"
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function getMiddle(s) {
+    if (s.length % 2 === 0) {
+        return s[s.length / 2 - 1] + s[s.length / 2];
+    } else {
+        return s[Math.floor(s.length / 2)];
+    }
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function getMiddle(s)
+{
+  return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+when getting this middle number from the odd set, 11/2=5.5... so i needed math.floor. Took a while to realize this.
+
+
+*/

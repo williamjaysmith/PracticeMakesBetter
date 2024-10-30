2a483106a50fe52ee7f31b97546a2ca2c52e93e4
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-10-30 07:46:30 -0500

    most often

---
+++ codewars/Kata 7/most-often.js
@@ -0,0 +1,49 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Given a string of characters, return the character that appears the most often.
+
+describe("Max Character", () => {
+ it("Should return max character", () => {
+  assert.equal(max("Hello World!"), "l");
+ });
+});
+
+No String or Array Methods (well brute force it first, but then no methods)! 
+
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function countChar(string) {
+    let charMap = {};
+    let maxValue = 0;
+    let maxChar = "";
+
+    for (char of string) {
+        charMap[char] = (charMap[char] || 0) + 1;
+    }
+
+    for (let key in charMap) {
+        if (charMap[key] > maxValue) {
+            maxValue = charMap[key];
+            maxChar = key;
+        }
+    }
+    return maxChar;
+}
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+
+
+
+*/

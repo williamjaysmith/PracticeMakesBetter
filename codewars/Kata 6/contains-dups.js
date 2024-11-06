8b1c63189ed59e589e19bff91b88993dec168af3
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-11-06 08:38:34 -0600

    contains dups

---
+++ codewars/Kata 6/contains-dups.js
@@ -0,0 +1,53 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+Example 1:
+Input: nums = [1,2,3,1]
+Output: true
+Explanation:
+The element 1 occurs at the indices 0 and 3.
+
+Example 2:
+Input: nums = [1,2,3,4]
+Output: false
+Explanation:
+All elements are distinct.
+
+Example 3:
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+
+var containsDuplicate = function (nums) {
+    let numMap = {};
+    for (let num of nums) {
+        if (numMap[num]) {
+            return true;
+        } else {
+            numMap[num] = 1;
+        }
+    }
+    return false;
+};
+
+console.log(containsDuplicate([1, 2, 3, 1]), containsDuplicate([1, 2, 3, 4]));
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+
+
+
+*/

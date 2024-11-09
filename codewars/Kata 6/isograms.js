5989aff6a0d94f8bd623bc4719e5b71e048772fb
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-11-09 12:27:15 -0600

    isograms

---
+++ codewars/Kata 6/isograms.js
@@ -0,0 +1,38 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function isIsogram(str) {
+    const strMap = {};
+    for (let char of str.toLowerCase()) {
+        if (strMap[char]) {
+            return false;
+        } else {
+            strMap[char] = true;
+        }
+    }
+    return true;
+}
+
+console.log(isIsogram("Dermatoglyphics"), isIsogram("aba"));
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function isIsogram(str){
+	return new Set(str.toUpperCase()).size == str.length;
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+reminded theres an easier way with new Set
+
+
+*/

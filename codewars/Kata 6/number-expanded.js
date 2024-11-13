d161bab0920615bb4e41af48bd1c839c135034a2
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-11-13 08:01:36 -0600

    number-expanded

---
+++ codewars/Kata 6/number-expanded.js
@@ -0,0 +1,47 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+   12 --> "10 + 2"
+   45 --> "40 + 5"
+70304 --> "70000 + 300 + 4"
+NOTE: All numbers will be whole numbers greater than 0.
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function expandedForm(num) {
+    let numArray = num.toString().split("");
+
+    let expandedArray = numArray.map((digit, index, array) => {
+        let placeValue = digit * Math.pow(10, array.length - index - 1);
+
+        return placeValue;
+    });
+
+    return expandedArray.filter((val) => val > 0).join(" + ");
+}
+
+console.log(expandedForm(70304));
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+const expandedForm = n => n.toString()
+                            .split("")
+                            .reverse()
+                            .map( (a, i) => a * Math.pow(10, i))
+                            .filter(a => a > 0)
+                            .reverse()
+                            .join(" + ");
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+
+
+
+*/

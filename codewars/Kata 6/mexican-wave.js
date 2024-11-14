93317d8a6ab8e40b3387d4e2b1918390eea20e2b
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-11-14 09:03:56 -0600

    mexican wave

---
+++ codewars/Kata 6/mexican-wave.js
@@ -0,0 +1,53 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+Task
+In this simple Kata your task is to create a function that turns a string into a Mexican Wave. You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up. 
+Rules
+ 1.  The input string will always be lower case but maybe empty.
+
+ 2.  If the character in the string is whitespace then pass over it as if it was an empty seat
+Example
+wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+function wave(str) {
+    let waveArray = [];
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === " ") continue;
+
+        let wave = str.slice(0, i) + str[i].toUpperCase() + str.slice(i + 1);
+        waveArray.push(wave);
+    }
+    return waveArray;
+}
+
+console.log(wave("hello"));
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+function wave(str){
+    let result = [];
+    
+    str.split("").forEach((char, index) => {
+        if (/[a-z]/.test(char)) {
+            result.push(str.slice(0, index) + char.toUpperCase() + str.slice(index + 1));
+        }
+    });
+    
+    return result;
+}
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+I got some solid practice with the slice method and had a little trouble with skipping spaces until i realized i was checking if (i the index) was a space... rather than str[i] 
+
+
+*/

b38f30c4bc627228f4384bb53608b9edc92fb6f1
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-11-17 17:28:12 -0600

    jewels and stones

---
+++ codewars/Kata 6/jewels-and-stones.js
@@ -0,0 +1,85 @@
+/*
+DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+ 
+
+Example 1:
+
+Input: jewels = "aA", stones = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: jewels = "z", stones = "ZZ"
+Output: 0
+ 
+
+Constraints:
+
+1 <= jewels.length, stones.length <= 50
+jewels and stones consist of only English letters.
+All the characters of jewels are unique.
+
+MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████*/
+
+var numJewelsInStones = function (jewels, stones) {
+    let listOfJewels = {};
+    let count = 0;
+
+    /* put jewels in an object */
+    for (let jewel of jewels) {
+        listOfJewels[jewel] = true;
+    }
+
+    /* check if stones are a jewel and then count */
+    for (let stone of stones) {
+        if (stone in listOfJewels) {
+            count++;
+        }
+    }
+    console.log(count);
+    console.table(listOfJewels);
+    return count;
+};
+
+numJewelsInStones("aA", "aAAbbbb");
+
+/*
+BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+const numJewelsInStones = (J, S) => {
+    const jewels = new Set(J)
+    return S.split('').reduce((res, s) => res + jewels.has(s), 0)
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+██████████████████████████████████████████████████████████
+
+
+
+
+*/

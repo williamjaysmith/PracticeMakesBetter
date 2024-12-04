dfe569c2843d29f8c10515dc0ace545a6138ebc4
williamjaysmith <164584554+williamjaysmith@users.noreply.github.com>
2024-12-04 08:47:21 -0600

    consecutive numbers

--- codewars/Kata 6/consecutive-numbers.js
+++ codewars/Kata 6/consecutive-numbers.js
@@ -2,59 +2,75 @@
 DESCRIPTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 ██████████████████████████████████████████████████████████
 
-Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+For a given string s find the character c (or C) with longest consecutive repetition and return:
 
-And a tower with 6 floors looks like this:
+[c, l]
+where l (or L) is the length of the repetition. If there are two or more characters with the same l return the first in order of appearance.
 
-[
-  "     *     ", 
-  "    ***    ", 
-  "   *****   ", 
-  "  *******  ", 
-  " ********* ", 
-  "***********"
-]
+For empty string return:
 
+["", 0]
 
 MY SOLUTION ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 ██████████████████████████████████████████████████████████*/
-
-function chritmasTreeBuilder(branches) {
-    let tree = "";
-    let stars = 1;
-
-    for (let i = 0; i < branches; i++) {
-        let spaces = " ".repeat(branches - i - 1);
-        tree += spaces + "*".repeat(stars) + spaces + "\n";
-        stars += 2;
-    }
-    let trunkWidth = Math.floor(branches / 3) || 1;
-    let trunkHeight = Math.floor(branches / 3);
-    let trunkSpaces = " ".repeat((stars - 1 - trunkWidth) / 2);
-
-    for (let i = 0; i < trunkHeight; i++) {
-        tree += trunkSpaces + "*".repeat(trunkWidth) + trunkSpaces + "\n";
+function longestRepetition(s) {
+    if (!s || s.length === 0) {
+        return ["", 0];
     }
 
+    let maxChar = "";
+    let maxCount = 0;
+
+    let currentChar = s[0];
+    let currentCount = 1;
+
+    for (let i = 1; i < s.length; i++) {
+        if (s[i] === currentChar) {
+            currentCount++;
+        } else {
+            if (currentCount > maxCount) {
+                maxChar = currentChar;
+                maxCount = currentCount;
+            }
+            currentChar = s[i];
+            currentCount = 1;
+        }
+    }
+
+    // Check the last streak
+    if (currentCount > maxCount) {
+        maxChar = currentChar;
+        maxCount = currentCount;
+    }
+
+    return [maxChar, maxCount];
 }
-      *
-     ***
-    *****
-   *******
-  *********
- ***********
-*************
-      **
-      **
-chritmasTreeBuilder(11);
-
-
 
 /*
 BEST PRACTICE ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 ██████████████████████████████████████████████████████████
 
+function longestRepetition(s) {
+  let count = 0;
+  let prevLetter = '';
+  
+  return s.toLowerCase().split('').reduce((acc, curr) => {
+    if(curr === prevLetter){
+      count++;
+    }
+    else{
+      count = 1;
+    }
 
+    if(count > acc[1]){
+      acc[1] = count;
+      acc[0] = curr;
+    }
+
+    prevLetter = curr;
+    return acc;
+  }, ['', 0]);
+}
 
 WHAT DID I LEARN? ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 ██████████████████████████████████████████████████████████
